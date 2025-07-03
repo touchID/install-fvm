c03ab2cc6630b9e9ed1996bf8f3a96adc16a6a3a
touchid <ios.7@qq.com>
2025-07-04 07:18:13 +0800

    Create Neat Download Manager.sh

---
+++ Neat Download Manager.sh
@@ -0,0 +1,5 @@
+# install ndm
+下载ndm
+open http://www.neatdownloadmanager.com/index.php/en/
+open http://www.neatdownloadmanager.com/file/NeatDM_setup.exe
+open http://www.neatdownloadmanager.com/file/NeatDMInstaller.dmg
